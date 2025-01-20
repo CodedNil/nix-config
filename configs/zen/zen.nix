4447274e29d8085c0aeb666c8c2fe3f60c745816
Dan <codednil@gmail.com>
2025-01-20 14:05:22 +0000

    Clean zen.nix

--- configs/zen/zen.nix
+++ configs/zen/zen.nix
@@ -11,112 +11,57 @@ let
   # Base directory within the theme store
   themeBaseDir = "${themeStoreRepo}/themes";
 
+  # Base path for the profiles
+  profileBasePath = ".zen/profiletest.default";
+
   # Function to gather theme CSS and generate import statements
-  gatherThemeData =
-    {
-      id,
-      name,
-      preferences ? true,
-    }:
-    {
-      cssImportLine = "@import url(\"file://${config.home.homeDirectory}/.zen/profiletest.default/chrome/zen-themes/${name}/chrome.css\");";
-      files =
-        [
-          {
-            source = "${themeBaseDir}/${id}/chrome.css";
-            target = ".zen/profiletest.default/chrome/zen-themes/${name}/chrome.css";
-          }
-          {
-            source = "${themeBaseDir}/${id}/readme.md";
-            target = ".zen/profiletest.default/chrome/zen-themes/${name}/readme.md";
-          }
-        ]
-        ++ (
-          if preferences then
-            [
-              {
-                source = "${themeBaseDir}/${id}/preferences.json";
-                target = ".zen/profiletest.default/chrome/zen-themes/${name}/preferences.json";
-              }
-            ]
-          else
-            [ ]
-        );
-    };
+  gatherThemeData = id: name: preferences: {
+    cssImportLine = "@import url(\"file://${config.home.homeDirectory}/${profileBasePath}/chrome/zen-themes/${name}/chrome.css\");";
+    files =
+      [
+        {
+          source = "${themeBaseDir}/${id}/chrome.css";
+          target = "${profileBasePath}/chrome/zen-themes/${name}/chrome.css";
+        }
+        {
+          source = "${themeBaseDir}/${id}/readme.md";
+          target = "${profileBasePath}/chrome/zen-themes/${name}/readme.md";
+        }
+      ]
+      ++ (
+        if preferences then
+          [
+            {
+              source = "${themeBaseDir}/${id}/preferences.json";
+              target = "${profileBasePath}/chrome/zen-themes/${name}/preferences.json";
+            }
+          ]
+        else
+          [ ]
+      );
+  };
 
   # List of themes
   themes = [
-    {
-      id = "f50841b2-5e4a-4534-985d-b7f7b96088c2";
-      name = "NoHighlightSplit";
-    }
-    {
-      id = "906c6915-5677-48ff-9bfc-096a02a72379";
-      name = "Floating Status Bar";
-    }
-    {
-      id = "d7076c31-f6c1-4f28-b2e8-15b95f5a3d6f";
-      name = "No Search Shortcut Icons";
-    }
-    {
-      id = "664c54f9-d97d-410b-a479-23dd8a08a628";
-      name = "Better Tab Indicators";
-      preferences = true;
-    }
-    {
-      id = "cfa711cf-e9f7-4c35-8289-3e7633f93565";
-      name = "Fluid URL";
-      preferences = true;
-    }
-    {
-      id = "4596d8f9-f0b7-4aeb-aa92-851222dc1888";
-      name = "Only Close On Hover";
-    }
-    {
-      id = "ad97bb70-0066-4e42-9b5f-173a5e42c6fc";
-      name = "SuperPins";
-      preferences = true;
-    }
-    {
-      id = "d93e67f8-e5e1-401e-9b82-f9d5bab231e6";
-      name = "Super Url Bar";
-      preferences = true;
-    }
-    {
-      id = "f4866f39-cfd6-4498-ab92-54213b8279dc";
-      name = "Animations Plus";
-    }
-    {
-      id = "fd24f832-a2e6-4ce9-8b19-7aa888eb7f8e";
-      name = "Quietify";
-    }
-    {
-      id = "1e86cf37-a127-4f24-b919-d265b5ce29a0";
-      name = "Cleaner Extension Menu";
-    }
-    {
-      id = "a6335949-4465-4b71-926c-4a52d34bc9c0";
-      name = "Better Find Bar";
-    }
-    {
-      id = "87196c08-8ca1-4848-b13b-7ea41ee830e7";
-      name = "Tab Preview Enhanced";
-    }
-    {
-      id = "ae7868dc-1fa1-469e-8b89-a5edf7ab1f24";
-      name = "Load Bar";
-    }
-    {
-      id = "253a3a74-0cc4-47b7-8b82-996a64f030d5";
-      name = "Floating History";
-    }
+    (gatherThemeData "f4866f39-cfd6-4498-ab92-54213b8279dc" "Animations Plus" false)
+    (gatherThemeData "a6335949-4465-4b71-926c-4a52d34bc9c0" "Better Find Bar" false)
+    (gatherThemeData "664c54f9-d97d-410b-a479-23dd8a08a628" "Better Tab Indicators" true)
+    (gatherThemeData "1e86cf37-a127-4f24-b919-d265b5ce29a0" "Cleaner Extension Menu" false)
+    (gatherThemeData "253a3a74-0cc4-47b7-8b82-996a64f030d5" "Floating History" false)
+    (gatherThemeData "906c6915-5677-48ff-9bfc-096a02a72379" "Floating Status Bar" false)
+    (gatherThemeData "cfa711cf-e9f7-4c35-8289-3e7633f93565" "Fluid URL" true)
+    (gatherThemeData "ae7868dc-1fa1-469e-8b89-a5edf7ab1f24" "Load Bar" false)
+    (gatherThemeData "f50841b2-5e4a-4534-985d-b7f7b96088c2" "NoHighlightSplit" false)
+    (gatherThemeData "d7076c31-f6c1-4f28-b2e8-15b95f5a3d6f" "No Search Shortcut Icons" false)
+    (gatherThemeData "4596d8f9-f0b7-4aeb-aa92-851222dc1888" "Only Close On Hover" false)
+    (gatherThemeData "fd24f832-a2e6-4ce9-8b19-7aa888eb7f8e" "Quietify" false)
+    (gatherThemeData "ad97bb70-0066-4e42-9b5f-173a5e42c6fc" "SuperPins" true)
+    (gatherThemeData "d93e67f8-e5e1-401e-9b82-f9d5bab231e6" "Super Url Bar" true)
+    (gatherThemeData "87196c08-8ca1-4848-b13b-7ea41ee830e7" "Tab Preview Enhanced" false)
   ];
 
-  # Process theme data to extract file mappings and CSS imports
-  processedThemes = map gatherThemeData themes;
-
   # Combine all import statements into a single CSS text
-  cssImportText = builtins.concatStringsSep "\n" (map (theme: theme.cssImportLine) processedThemes);
+  cssImportText = builtins.concatStringsSep "\n" (map (theme: theme.cssImportLine) themes);
 
   # Merge all theme file configurations
   themeFileEntries = builtins.foldl' (
@@ -133,7 +78,7 @@ let
         };
       }) theme.files
     )
-  ) { } processedThemes;
+  ) { } themes;
 
 in
 {
@@ -146,12 +91,17 @@ in
     zen_profile = {
       enable = true;
       source = ./configs/zen/user.js;
-      target = ".zen/profiletest.default/user.js";
+      target = "${profileBasePath}/user.js";
     };
     zen_themes_json = {
       enable = true;
       source = ./configs/zen/zen-themes.json;
-      target = ".zen/profiletest.default/zen-themes.json";
+      target = "${profileBasePath}/zen-themes.json";
+    };
+    zen_themes_css = {
+      enable = true;
+      source = cssImportText;
+      target = "${profileBasePath}/chrome/zen-themes.css";
     };
   } // themeFileEntries;
 }
